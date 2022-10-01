b0391b3a6684bd9e31088d6702799669a445c756
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-10-01 10:25:32 -0300

    chore: change to system theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class" enableSystem>
+    <ThemeProvider attribute="class" enableSystem>
       <Component {...pageProps} />
     </ThemeProvider>
   )
