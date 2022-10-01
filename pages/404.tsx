7698447e08bcf6ea1a06581466f7c5420832c175
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-10-01 12:35:10 -0300

    chore: improve landing page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import useWindowDimensions from '@/hooks/useWindowDimensions'
 import Page from '@/components/layout/Page'
-import FourOFour from '@/public/images/404.jpg'
+import ICEYE from '@/public/images/iceye.jpg'
 
 export default function NotFound() {
   const { width, height } = useWindowDimensions()
@@ -11,7 +11,7 @@ export default function NotFound() {
   return (
     <Page className="overflow-hidden">
       <section className="flex items-center justify-around">
-        <div className="dark:bg-terra-o-black min-w-[44vw] bg-terra-o-white flex items-center flex-col gap-y-6 justify-center h-[88vh]">
+        <div className="w-screen md:w-[44vw] flex items-center flex-col gap-y-6 justify-center h-[88vh]">
           <h1 className="text-6xl text-transparent bg-clip-text bg-gradient-to-r from-terra-o-blue-100 to-terra-o-green-200 font-terra-o">
             404
           </h1>
@@ -20,23 +20,24 @@ export default function NotFound() {
           </p>
           <Link href="/">
             <button className="mt-2 px-6 py-4 border-2 border-terra-o-black dark:text-terra-o-white dark:hover:text-terra-o-black hover:text-terra-o-white text-terra-o-black hover:bg-terra-o-black dark:border-terra-o-white dark:hover:bg-terra-o-white transition-colors duration-100">
-              <a className="font-sans px-4">Go back to Terra O</a>
+              <a className="font-sans px-4">Go back to Earth</a>
             </button>
           </Link>
         </div>
-        <div className="hidden md:block max-h-[88vh] max-w-[44vw]">
+        <div className="hidden md:block max-h-[88vh] w-[44vw]">
           {width && height && (
             <Link href="https://www.iceye.com/press/press-releases/iceye-secures-usd-1-2m-in-funding-from-european-seraphim-capital">
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 title="Image by ICEYE"
+                className="flex justify-start items-center"
               >
                 <Image
-                  src={FourOFour}
-                  alt="404"
+                  src={ICEYE}
+                  alt="ICEYE"
                   layout="intrinsic"
-                  width={width * 0.44}
+                  width={width * 0.38}
                   height={height * 0.88}
                 />
               </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,169 +1,69 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import useWindowDimensions from '@/hooks/useWindowDimensions'
 import Page from '@/components/layout/Page'
+import ARSET from '@/public/images/arset.png'
+import O from '@/public/images/o.png'
 
 export default function Index() {
+  const { width, height } = useWindowDimensions()
+
   return (
     <Page>
-      <div className="flex justify-center">
-        <p className="max-w-[90vw] font-sans text-md text-terra-o-black dark:text-terra-o-white text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras laoreet
-          massa eros, vel rhoncus arcu ornare tincidunt. Praesent venenatis orci
-          sed ornare luctus. Maecenas pellentesque condimentum enim ac mollis.
-          Donec sagittis, nulla at sagittis condimentum, nunc neque sagittis
-          ante, tempus congue quam tortor nec nulla. Nulla cursus erat vel dui
-          venenatis venenatis. Vestibulum a sapien ultrices libero imperdiet
-          faucibus. Sed in dignissim ligula. Aliquam blandit tempus justo, at
-          iaculis sapien eleifend sed. Nunc vitae scelerisque tortor. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum
-          venenatis diam. Nunc at neque ac sapien pellentesque facilisis. Donec
-          sollicitudin accumsan sagittis. Integer tristique urna in lacus
-          gravida viverra. In vel eros ac odio aliquam viverra. Aliquam tellus
-          leo, consequat a porta eget, lacinia luctus dolor. Aenean convallis
-          fermentum molestie. Suspendisse faucibus eleifend libero, cursus
-          aliquam quam tristique ut. Pellentesque sem metus, euismod sit amet
-          volutpat vitae, pharetra commodo justo. Fusce posuere ex ac ante
-          tristique posuere. Integer vel est magna. Cras et metus dolor.
-          Vestibulum sit amet consectetur libero, at euismod tellus. Nam at sem
-          id dolor accumsan varius. Quisque vulputate ac magna vitae
-          scelerisque. Donec accumsan metus in dapibus tempus. Etiam magna odio,
-          commodo sed vestibulum et, commodo eget libero. Mauris non lacinia mi.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas
-          euismod, nisl non accumsan faucibus, diam urna feugiat enim, ac
-          blandit nibh enim ac turpis. Quisque ac lectus et massa egestas
-          sagittis. Curabitur ac sapien congue orci maximus hendrerit. Fusce nec
-          accumsan nibh. Donec et pretium dui, eu dictum dolor. Praesent est
-          erat, euismod in lorem quis, laoreet maximus enim. Maecenas bibendum
-          ex sit amet molestie efficitur. Curabitur varius enim eros, hendrerit
-          commodo nulla finibus quis. In volutpat laoreet purus, ut pretium
-          turpis volutpat ac. Quisque magna magna, vulputate at viverra in,
-          laoreet sed est. In hac habitasse platea dictumst. Sed ornare congue
-          molestie. Sed nisi orci, maximus quis felis vel, condimentum mattis
-          urna. Pellentesque eget lacus eget elit tincidunt pulvinar sed in
-          diam. Fusce pulvinar egestas vehicula. Donec porta aliquam quam, quis
-          tristique dolor tincidunt non. Praesent sit amet elementum magna.
-          Donec ac mi mauris. Mauris ut risus a est euismod sagittis id ut ante.
-          Nam fringilla quam hendrerit, tincidunt enim vel, sodales eros. Donec
-          nec posuere tortor, ut tristique mi. Cras blandit scelerisque enim.
-          Quisque id feugiat dolor, sed pharetra purus. Suspendisse tempor
-          feugiat ultrices. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Donec fringilla quis est vel auctor. Suspendisse ullamcorper
-          libero nisi, in rhoncus risus finibus ut. Sed egestas enim sit amet
-          magna interdum, id interdum nulla imperdiet. Nullam tristique
-          consectetur urna non lobortis. Vestibulum et porttitor tellus. Aliquam
-          semper eu velit sit amet dapibus. Curabitur sit amet ultricies dolor.
-          Praesent eu nisi vitae lorem faucibus hendrerit. Maecenas vel magna
-          elit. Duis volutpat dignissim lectus quis varius. Mauris porttitor
-          mauris sit amet erat malesuada mollis. Mauris sem arcu, feugiat id
-          pretium nec, luctus ac sapien. Sed sed erat sollicitudin, interdum
-          lacus eu, laoreet elit. Vivamus maximus quam eu nisl ultricies
-          tristique ac varius felis. Aliquam erat volutpat. Aliquam fermentum
-          magna vel augue lacinia ullamcorper. Morbi scelerisque ullamcorper
-          dictum. Fusce eget lacinia enim, sed suscipit velit. Maecenas posuere
-          nunc ut vestibulum elementum. Proin ullamcorper, orci at ornare
-          ultrices, risus lacus malesuada dolor, sed blandit tellus purus nec
-          mi. Vivamus diam urna, sodales eget urna vitae, ornare commodo ex.
-          Pellentesque ultrices auctor ex quis mollis. Nullam dignissim dictum
-          est, vel lacinia purus tempor a. Aenean vitae odio nisl. Mauris non
-          tellus et ligula viverra interdum. Morbi sodales vulputate tempor. Sed
-          urna nisi, blandit in diam id, hendrerit sollicitudin ipsum. Morbi
-          iaculis venenatis magna id luctus. Nullam urna nibh, hendrerit non
-          condimentum nec, porta pretium nunc. Morbi sit amet porta lorem. Morbi
-          egestas felis vel vestibulum consequat. Mauris aliquam enim arcu, sit
-          amet consequat eros feugiat eget. In quis massa in enim viverra
-          lobortis. Cras eu aliquam velit, in varius purus. Duis vitae metus
-          vestibulum, luctus diam nec, posuere libero. Suspendisse placerat nibh
-          in odio fringilla, placerat feugiat felis dignissim. Nulla id laoreet
-          mi. Sed scelerisque vehicula erat id eleifend. Nullam pretium mi vitae
-          ante egestas, nec pulvinar erat placerat. Nulla eu feugiat est.
-          Phasellus sapien ex, facilisis eget volutpat id, tincidunt et justo.
-          Curabitur vestibulum vulputate elit, condimentum viverra eros. Mauris
-          sit amet lectus et ex sodales laoreet. Proin dapibus, tortor quis
-          ornare dictum, odio lectus fringilla turpis, et pharetra justo risus
-          ac erat. Donec vel efficitur mi. Sed et metus eget mi scelerisque
-          porttitor. Mauris nec ipsum auctor ante porta efficitur. Nullam
-          fermentum blandit velit, quis consectetur purus fermentum nec. Nam
-          bibendum, dui nec fringilla rhoncus, elit metus bibendum ipsum, non
-          iaculis diam dolor in dui. Etiam luctus diam sit amet dolor dapibus,
-          in venenatis magna hendrerit. Nam sodales nisi sit amet dolor
-          vulputate laoreet. Sed dictum nulla commodo leo tempor, sed accumsan
-          risus lacinia. Quisque blandit urna ut justo molestie, in maximus erat
-          dignissim. Proin et fermentum justo. Fusce sed hendrerit nulla. Proin
-          imperdiet ultrices urna sed malesuada. Donec aliquet leo et metus
-          molestie congue. Aenean tincidunt quam a odio scelerisque maximus.
-          Vestibulum porttitor ipsum eget est tempor vehicula. Mauris imperdiet
-          ex sit amet nibh semper volutpat. Aliquam erat volutpat. Aliquam
-          fermentum magna vel augue lacinia ullamcorper. Morbi scelerisque
-          ullamcorper dictum. Fusce eget lacinia enim, sed suscipit velit.
-          Maecenas posuere nunc ut vestibulum elementum. Proin ullamcorper, orci
-          at ornare ultrices, risus lacus malesuada dolor, sed blandit tellus
-          purus nec mi. Vivamus diam urna, sodales eget urna vitae, ornare
-          commodo ex. Pellentesque ultrices auctor ex quis mollis. Nullam
-          dignissim dictum est, vel lacinia purus tempor a. Aenean vitae odio
-          nisl. Mauris non tellus et ligula viverra interdum. Morbi sodales
-          vulputate tempor. Sed urna nisi, blandit in diam id, hendrerit
-          sollicitudin ipsum. Morbi iaculis venenatis magna id luctus. Nullam
-          urna nibh, hendrerit non condimentum nec, porta pretium nunc. Morbi
-          sit amet porta lorem. Morbi egestas felis vel vestibulum consequat.
-          Mauris aliquam enim arcu, sit amet consequat eros feugiat eget. In
-          quis massa in enim viverra lobortis. Cras eu aliquam velit, in varius
-          purus. Duis vitae metus vestibulum, luctus diam nec, posuere libero.
-          Suspendisse placerat nibh in odio fringilla, placerat feugiat felis
-          dignissim. Nulla id laoreet mi. Sed scelerisque vehicula erat id
-          eleifend. Nullam pretium mi vitae ante egestas, nec pulvinar erat
-          placerat. Nulla eu feugiat est. Phasellus sapien ex, facilisis eget
-          volutpat id, tincidunt et justo. Curabitur vestibulum vulputate elit,
-          condimentum viverra eros. Mauris sit amet lectus et ex sodales
-          laoreet. Proin dapibus, tortor quis ornare dictum, odio lectus
-          fringilla turpis, et pharetra justo risus ac erat. Donec vel efficitur
-          mi. Sed et metus eget mi scelerisque porttitor. Mauris nec ipsum
-          auctor ante porta efficitur. Nullam fermentum blandit velit, quis
-          consectetur purus fermentum nec. Nam bibendum, dui nec fringilla
-          rhoncus, elit metus bibendum ipsum, non iaculis diam dolor in dui.
-          Etiam luctus diam sit amet dolor dapibus, in venenatis magna
-          hendrerit. Nam sodales nisi sit amet dolor vulputate laoreet. Sed
-          dictum nulla commodo leo tempor, sed accumsan risus lacinia. Quisque
-          blandit urna ut justo molestie, in maximus erat dignissim. Proin et
-          fermentum justo. Fusce sed hendrerit nulla. Proin imperdiet ultrices
-          urna sed malesuada. Donec aliquet leo et metus molestie congue. Aenean
-          tincidunt quam a odio scelerisque maximus. Vestibulum porttitor ipsum
-          eget est tempor vehicula. Mauris imperdiet ex sit amet nibh semper
-          volutpat. Aliquam erat volutpat. Aliquam fermentum magna vel augue
-          lacinia ullamcorper. Morbi scelerisque ullamcorper dictum. Fusce eget
-          lacinia enim, sed suscipit velit. Maecenas posuere nunc ut vestibulum
-          elementum. Proin ullamcorper, orci at ornare ultrices, risus lacus
-          malesuada dolor, sed blandit tellus purus nec mi. Vivamus diam urna,
-          sodales eget urna vitae, ornare commodo ex. Pellentesque ultrices
-          auctor ex quis mollis. Nullam dignissim dictum est, vel lacinia purus
-          tempor a. Aenean vitae odio nisl. Mauris non tellus et ligula viverra
-          interdum. Morbi sodales vulputate tempor. Sed urna nisi, blandit in
-          diam id, hendrerit sollicitudin ipsum. Morbi iaculis venenatis magna
-          id luctus. Nullam urna nibh, hendrerit non condimentum nec, porta
-          pretium nunc. Morbi sit amet porta lorem. Morbi egestas felis vel
-          vestibulum consequat. Mauris aliquam enim arcu, sit amet consequat
-          eros feugiat eget. In quis massa in enim viverra lobortis. Cras eu
-          aliquam velit, in varius purus. Duis vitae metus vestibulum, luctus
-          diam nec, posuere libero. Suspendisse placerat nibh in odio fringilla,
-          placerat feugiat felis dignissim. Nulla id laoreet mi. Sed scelerisque
-          vehicula erat id eleifend. Nullam pretium mi vitae ante egestas, nec
-          pulvinar erat placerat. Nulla eu feugiat est. Phasellus sapien ex,
-          facilisis eget volutpat id, tincidunt et justo. Curabitur vestibulum
-          vulputate elit, condimentum viverra eros. Mauris sit amet lectus et ex
-          sodales laoreet. Proin dapibus, tortor quis ornare dictum, odio lectus
-          fringilla turpis, et pharetra justo risus ac erat. Donec vel efficitur
-          mi. Sed et metus eget mi scelerisque porttitor. Mauris nec ipsum
-          auctor ante porta efficitur. Nullam fermentum blandit velit, quis
-          consectetur purus fermentum nec. Nam bibendum, dui nec fringilla
-          rhoncus, elit metus bibendum ipsum, non iaculis diam dolor in dui.
-          Etiam luctus diam sit amet dolor dapibus, in venenatis magna
-          hendrerit. Nam sodales nisi sit amet dolor vulputate laoreet. Sed
-          dictum nulla commodo leo tempor, sed accumsan risus lacinia. Quisque
-          blandit urna ut justo molestie, in maximus erat dignissim. Proin et
-          fermentum justo. Fusce sed hendrerit nulla. Proin imperdiet ultrices
-          urna sed malesuada. Donec aliquet leo et metus molestie congue. Aenean
-          tincidunt quam a odio scelerisque maximus. Vestibulum porttitor ipsum
-          eget est tempor vehicula. Mauris imperdiet ex sit amet nibh semper
-          volutpat.
-        </p>
-      </div>
+      <section className="flex items-center justify-around">
+        <div className="hidden md:block max-h-[88vh] w-[44vw]">
+          {width && height && (
+            <Link href="https://appliedsciences.nasa.gov/what-we-do/capacity-building/arset">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Image by ARSET"
+                className="flex justify-end items-center"
+              >
+                <Image
+                  src={ARSET}
+                  alt="ARSET"
+                  layout="intrinsic"
+                  width={width * 0.38}
+                  height={height * 0.88}
+                />
+              </a>
+            </Link>
+          )}
+        </div>
+        <div className="dark:bg-terra-o-black w-screen md:w-[36vw] bg-terra-o-white flex items-start flex-col gap-y-6 justify-start h-[88vh]">
+          <div className="text-left font-sans text-6xl dark:text-terra-o-white text-terra-o-black flex gap-y-6 flex-col">
+            <span className="font-terra-o text-8xl dark:text-terra-o-white text-terra-o-terra flex items-center justify-start gap-x-6">
+              Terra
+              <Image src={O} alt="O" width={92} height={92} />
+            </span>
+            <h2 className="text-terra-o-gray-300">
+              Your gateway to acknowledge{' '}
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-terra-o-blue-50 to-terra-o-green-300 font-sans">
+                Earth.
+              </span>
+            </h2>
+            <h2>Observe. Visualize. Extend.</h2>
+            <p className="text-2xl dark:text-terra-o-gray-200 text-terra-o-gray-300">
+              Terra O is a gateway that provides capacity building resources to
+              Earth Observation.
+            </p>
+          </div>
+          <div className="flex gap-x-6">
+            <Link href="/quickstart">
+              <button className="mt-2 px-6 py-4 border-2 border-terra-o-black dark:text-terra-o-white dark:hover:text-terra-o-black hover:text-terra-o-white text-terra-o-black hover:bg-terra-o-black dark:border-terra-o-white dark:hover:bg-terra-o-white transition-colors duration-100">
+                <a className="font-sans px-4">Quickstart</a>
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="mt-2 px-6 py-4 border-2 border-terra-o-black dark:bg-terra-o-white dark:text-terra-o-black dark:hover:text-terra-o-white bg-terra-o-black hover:text-terra-o-black text-terra-o-white hover:bg-terra-o-white dark:border-terra-o-white dark:hover:bg-terra-o-black transition-colors duration-100">
+                <a className="font-sans px-4">Custom search</a>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </Page>
   )
 }
